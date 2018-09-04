ce300c1c535bec6f8b56484078a0dea08119331c
Cristian Consonni <cristian@balist.es>
2018-09-04 22:16:50 +0200

    Add __init__.py in the main module directory, minor fixes to setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 """graphsnapshot
     ~~~~~~~~
-     Process links and other data extracted from the Wikipedia dump.
+    Process links and other data extracted from the Wikipedia dump.
 
 """
 
@@ -12,7 +12,7 @@ setup(
     author='Cristian Consonni',
     author_email='cris' 'tian.con' 'sonni' '<a' 't>' 'uni' 'tn' '<d' 'ot>it',
     license='GPL3',
-    description=' Process links and other data extracted from the Wikipedia dump.',
+    description='Process links and other data extracted from the Wikipedia dump.',
     long_description=__doc__,
     url='https://github.com/CristianCantoro/wikipedia-graphsnapshot',
     packages=find_packages(),
@@ -22,16 +22,22 @@ setup(
         ],
     },
     install_requires=[
+        'arrow==0.7.0',
+        'compressed-stream==0.0.2',
+        'jsonable==0.3.1',
         'Mako==1.0.2',
+        'MarkupSafe==0.23',
         'mediawiki-utilities==0.4.18',
         'mwcites==0.2.0',
         'mwcli==0.0.1',
+        'mwparserfromhell==0.4.2',
         'mwtypes==0.2.0',
         'mwxml==0.2.0',
-        'regex==2015.9.28',
+        'regex==2018.8.17',
         'more-itertools==2.2',
         'fuzzywuzzy==0.8.0',
         'python-Levenshtein==0.12.0',
+        'requests==2.9.1',
         'typing==3.5.0.1',
     ],
     zip_safe=False,
