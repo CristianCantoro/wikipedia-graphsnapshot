56cac103d094a449c93f32cc29865627b88ab25f
Cristian Consonni <cristian@balist.es>
2018-09-23 20:13:39 +0200

    Continue implementing extraction_comparator.py: we can sleect which files to compare, now we just need to compare them line by line

--- graphsnapshot/processors/extraction_comparator.py
+++ graphsnapshot/processors/extraction_comparator.py
@@ -6,6 +6,7 @@ The output format is csv.
 
 import os
 import csv
+import glob
 import mwxml
 import arrow
 import regex as re
@@ -41,10 +42,10 @@ re_chunk = re.compile(chunk_regex, re.IGNORECASE | re.DOTALL)
 #   * enwiki-20180301-pages-meta-history1.xml-p10p2115.7z.features.xml.gz
 #     {lang}wiki-{date}-pages-meta-history{historyno}.xml
 #       -p{pageid_first}p{pageid_last}.{dumpext}.features.xml.{ext}
-new_chunk_glob = '{lang}wiki-{date}'
-new_chunk_glob += '-pages-meta-history{historyno}.xml'
-new_chunk_glob += '-p{pageid_first}p{pageid_last}.{dumpext}'
-new_chunk_glob += '.features.xml.{ext}'
+new_chunk_pattern = '{lang}wiki-{date}'
+new_chunk_pattern += '-pages-meta-history{historyno}.xml'
+new_chunk_pattern += '-p{pageid_first}p{pageid_last}.{dumpext}'
+new_chunk_pattern += '.features.xml.{ext}'
 
 
 Revision = NamedTuple('Revision', [
@@ -61,21 +62,23 @@ Page = NamedTuple('Page', [
     ('revision', Revision),
 ])
 
-# - Redirect:
-#   - Page:
-#     - page_id
-#     - page_title
-#     - Revision:
-#       - revision_id
-#       - revision_parent_id
-#       - revision_timestamp,
-#       - revision_minor
-#   - target
-#   - tosection
-Redirect = NamedTuple('Redirect', [
-    ('page', Page),
-    ('target', str),
-    ('tosection', str),
+
+# - Chunk:
+#   - lang
+#   - date
+#   - historyno
+#   - pageid_first
+#   - pageid_last
+#   - dumpext
+#   - ext
+Chunkfile = NamedTuple('Chunkfile', [
+    ('lang', str),
+    ('date', str),
+    ('historyno', int),
+    ('pageid_first', int),
+    ('pageid_last', int),
+    ('dumpext', str),
+    ('ext', str),
 ])
 
 
@@ -107,82 +110,234 @@ stats_template = '''
 '''
 
 
-def read_snapshot_pages(
-    input_file_path: Iterable[list]) -> Mapping:
-
-    snapshot = fu.open_csv_file(str(input_file_path))
-    snapshot_reader = csv.reader(snapshot)
-
-    title2id = dict()
-
-    counter = 0
-    print('\nReading snapshot ', end=' ')
-    for line in snapshot_reader:
-        if counter % NLINES == 0:
-            utils.dot()
-        counter = counter + 1
-
-        page_title = line[1]
-        page_id = int(line[0])
-
-        title2id[page_title] = page_id
-
-    return title2id
-
-
-def normalize_title(title: str) -> str:
-    if len(title) > 1:
-        title = title[0].upper() + title[1:]
-    elif len(title) == 1:
-        title = title[0].upper()
-
-    return title.replace('_', ' ')
-
-
+# def process_lines(
+#         dump: Iterable[list],
+#         stats: Mapping,
+#         selected_chunks: Iterable[list],
+#         ignore_newer: bool) -> Iterator[list]:
 def process_lines(
         dump: Iterable[list],
+        timestamps: Iterable[arrow.arrow.Arrow],
         stats: Mapping,
-        snapshot_title2id: Mapping,
-        redirects_history: Mapping) -> Iterator[list]:
+        only_last_revision: bool,
+        skip_header: bool) -> Iterator[list]:
     """Assign each revision to the snapshot or snapshots to which they
        belong.
     """
 
     # skip header
-    # header = next(dump)
-    next(dump)
-    header = csv_header_input
+    if skip_header:
+        next(dump)
 
+    header = csv_header
+
+    old_page = None
+    old_prevpage = None
+    new_page = None
+    new_prevpage = None
+
+    old_rev_prev = None
+    old_rev = None
+    new_rev_prev = None
+    new_rev = None
+
+    old_is_last_rev = None
+    new_is_last_rev = None
+
+    break_flag = False
+
+    i = 0
+    page_revisions = []
     counter = 0
-    print('\nProcess snapshot ', end=' ')
-    for snapshot_page in dump:
-        if counter % NLINES == 0:
-            utils.dot()
 
-        counter = counter + 1
-        # get only page id and page title
-        resolved = resolve_redirect(page=snapshot_page,
-                                    original_page=snapshot_page,
-                                    stats=stats,
-                                    snapshot_title2id=snapshot_title2id,
-                                    redirects_history=redirects_history,
-                                    count_recursive_calls=0,
-                                    )
+    # equivalent to
+    # for revision in dump:
+    while True:
+        if break_flag:
+            break
 
-        stats['performance']['pages_analyzed'] += 1
+        old_rev_prev = revision
+        old_rev = next(dump, None)
 
-        yield snapshot_page + resolved
+        if old_rev is None:
+                old_rev = old_rev_prev
+                old_is_last_revision = True
+
+        # read the line in a StringIO object and parse it with the csv module
+        try:
+            old_rev_parsed = [l for l in csv.reader(StringIO(old_rev))][0]
+        except (csv.Error, TypeError) as err:
+            continue
+
+        old_revdata = dict(zip(header, old_rev_parsed))
+        old_page = Page(
+            old_revdata['page_id'],
+            old_revdata['page_title'],
+            Revision(old_revdata['revision_id'],
+                     old_revdata['revision_parent_id'],
+                     arrow.get(old_revdata['revision_timestamp'])
+                     ))
+
+        if old_prevpage is None or \
+                old_prevpage.id != old_page.id:
+            # we are starting now or we have a new page
+            counter = 0
+            utils.log("Processing", dump_page.title)
+            stats['performance']['pages_analyzed'] += 1
+
+        if old_prevpage is None or \
+                old_prevpage.revision.id != old_page.revision.id:
+            stats['performance']['revisions_analyzed'] += 1
+
+        if not old_is_last_rev and \
+                (old_prevpage is None or old_prevpage.id == old_page.id):
+            # it is not the last revision, futhermore two cases:
+            #   * dump_prevpage is None: we are reading the first line of the
+            #     dump file
+            #   * dump_prevpage.id == dump_page.id we are reading a page whose
+            #     is is the same as the previous one we read
+            # so, either we just started or we are in the middle of the
+            # history of a page. What we do is we just add the revision to the
+            # list.
+
+            counter = counter + 1
+
+            if counter % NPRINTREVISION == 1:
+                utils.dot()
+
+            old_page_revisions.append(dump_page)
+            old_prevpage = old_page
+
+        else:
+            # cases:
+            #   * this is the last revision of the dump
+            #     (is_last_revision is True)
+            #   * we have changed to a new page (dump_prevpage is not None
+            #     and dump_prevpage.id != dump_page.id)
+
+            # sort all the revision by timestamp (they are not guaranted to be
+            # ordered)
+            sorted_revisions = sorted(page_revisions,
+                                      key=lambda pg: pg.revision.timestamp)
+
+            # if we only want the sorted_revisions list is limited to the
+            # last element.
+
+            # Note: don't try to bee to smart and think that one can skip
+            # reading all revisions and just take the last that is encountered,
+            # that is when page.id changes, i.e.
+            #     when dump_prevpage.id != dump_page.id
+            # because as said in the previous comment we are not assured that
+            # all revisions will be in the correct order, so we still need to
+            # collect them all, sort them and take the last one.
+            if only_last_revision:
+                sorted_revisions = [sorted_revisions[-1]]
+
+            # we are not interested to the new page for the moment, put it in
+            # the list
+            dump_prevpage = dump_page
+            page_revisions = [dump_page]
+
+            i = 0
+            j = 0
+            prevpage = None
+            while j < len(sorted_revisions):
+                page = sorted_revisions[j]
+
+                ct = page.revision.timestamp
+                pt = prevpage.revision.timestamp if prevpage else EPOCH
+
+                while i < len(timestamps):
+                    ts = timestamps[i]
+
+                    if not prevpage:
+                        # page contains the first revision for this page
+
+                        if ct > ts:
+                            # the page did not exist at the time
+                            # check another timestamp
+                            # print("ct {} > ts {}".format(ct, ts))
+
+                            i = i + 1
+                            continue
+
+                        else:
+                            # ct <= ts
+                            # check another revision
+                            # print("ct {} <= ts {}".format(ct, ts))
+
+                            # update step
+                            prevpage = page
+                            j = j + 1
+
+                            if j < len(sorted_revisions):
+                                break
+                    else:
+
+                        if pt > ts:
+                            # check the other timestamps
+                            # print("pt {} > ts {}" .format(pt, ts))
+
+                            i = i + 1
+                            continue
+
+                        elif ct > ts:
+                            # the previous revision is in the snapshot
+                            # check another timestamp
+                            # print("ct {} > ts {}".format(ct, ts))
+
+                            i = i + 1
+
+                            # print("{} -> {}".format(prevpage, ts), end='')
+                            # print(" - j: {}".format(j))
+                            yield (prevpage, ts)
+
+                            continue
+
+                        else:
+                            # check another revision
+                            # print("ct {} <= ts {}, pt {} <= ts {}"
+                            #       .format(ct, ts, pt, ts))
+
+                            # update step
+                            prevpage = page
+                            j = j + 1
+                            if j < len(sorted_revisions):
+                                break
+
+                    if j >= len(sorted_revisions):
+                        i = i + 1
+
+                        # print("--- {} -> {}".format(page, ts), end='')
+                        # print(" - j: {}".format(j))
+                        yield (page, ts)
+
+            if is_last_revision:
+                break_flag = True
+
+        if not is_last_revision:
+            stats['performance']['link_analyzed'] += 1
 
 
 def configure_subparsers(subparsers):
     """Configure a new subparser ."""
+    def is_dir(value):
+        path_value = pathlib.Path(value)
+        if not path_value.is_dir():
+            import argparse
+            raise argparse.ArgumentTypeError(
+                "{} is not a directory".format(value))
+
+        return path_value
+
+
     parser = subparsers.add_parser(
         'compare-extractions',
         help='Compare extractions.',
     )
     parser.add_argument(
         '--new-extractions-dir',
-        type=pathlib.Path,
+        type=is_dir,
         help='Directory with the new extractions.'
     )
 
@@ -207,6 +362,55 @@ def configure_subparsers(subparsers):
     )
 
     parser.set_defaults(func=main)
+
+
+def idtoint(pageid):
+    return int(pageid.lstrip('0'))
+
+
+def extract_name_elements(re_chunk_match):
+    lang = re_chunk_match.group(1)
+    date = re_chunk_match.group(2)
+    historyno = idtoint(re_chunk_match.group(3))
+    pageid_first = idtoint(re_chunk_match.group(4))
+    pageid_last = idtoint(re_chunk_match.group(5))
+    dumpext = re_chunk_match.group(6)
+    ext = ''
+    try:
+        # ignore groups 7 and 8
+        ext = re_chunk_match.group(9)
+    except IndexError:
+        pass
+
+    return Chunkfile(lang,
+                     date,
+                     historyno,
+                     pageid_first,
+                     pageid_last,
+                     dumpext,
+                     ext)
+
+
+def select_intervals(old_interval, new_intervals_list):
+    old_start = old_interval[0]
+    old_end = old_interval[1]
+
+    intervals = set()
+    for start, end in new_intervals_list:
+        if (start <= old_start and end >= old_start) or \
+                (start <= old_start and end >= old_start) or \
+                (end >= old_end and start <= old_end):
+            intervals.add((start, end))
+
+    return intervals
+
+
+def sort_chunks(chunk_path):
+    chunk_basename = os.path.basename(chunk_path)
+    match = re_chunk.match(chunk_basename)
+    achunk = extract_name_elements(match)
+
+    return achunk.pageid_first
 
 
 def main(
@@ -250,43 +454,106 @@ def main(
 
     base_match = re_chunk.match(basename)
     if base_match:
-        base_chunk_lang = base_match.group(1)
-        base_chunk_date = base_match.group(2)
-        base_chunk_historyno = base_match.group(3)
-        base_chunk_pageid_first = int(base_match.group(4).lstrip('0'))
-        base_chunk_pageid_last = int(base_match.group(5).lstrip('0'))
-        base_chunk_dumpext = base_match.group(6)
-
-        base_chunk_ext = ''
-        try:
-            # ignore groups 7 and 8
-            base_chunk_ext = base_match.group(9)
-        except IndexError:
-            pass
+        base_chunk = extract_name_elements(base_match)
+    else:
+        msg = ("Unexpected name for input file: {filename} ({path})"
+               .format(filename=basename, path=inputfile_full_path))
+        raise ValueError(msg)
+        del msg
 
     if args.old_extraction_date is None:
-        old_extraction_date = arrow.get(base_chunk_date, 'YYYYMMDD')
+        old_extraction_date = arrow.get(base_chunk.date, 'YYYYMMDD')
     else:
         old_extraction_date = arrow.get(args.old_extraction_date)
 
     assert (old_extraction_date > DATE_START and \
                 old_extraction_date < DATE_NOW)
 
-    import ipdb; ipdb.set_trace()
+    # {lang}wiki-{date}-pages-meta-history{historyno}.xml
+    #   -p{pageid_first}p{pageid_last}.{dumpext}.features.xml.{ext}
+    new_chunk_glob = (new_chunk_pattern
+                      .format(lang=base_chunk.lang,
+                               date='*',
+                               historyno='*',
+                               pageid_first='*',
+                               pageid_last='*',
+                               dumpext='*',
+                               ext='*')
+                      )
 
-    if args.old_extraction_date is None:
-        new_extraction_date = arrow.get(base_chunk_date, 'YYYYMMDD')
+    new_chunks_ids = []
+    new_chunks_dates = set()
+    for cf in glob.glob(os.path.join(new_extractions_dir.as_posix(),
+                                     new_chunk_glob
+                                     )):
+
+        cf_basename = os.path.basename(cf)
+        new_match = re_chunk.match(cf_basename)
+        if new_match:
+            new_chunk = extract_name_elements(new_match)
+            new_chunks_ids.append((new_chunk.pageid_first,
+                                  new_chunk.pageid_last)
+                                 )
+            new_chunks_dates.add(new_chunk.date)
+        else:
+            msg = ("Unexpected name for new chunk: {filename} ({path})"
+                   .format(filename=cf_basename,
+                           path=cf))
+            raise ValueError(msg)
+            del msg
+    new_chunks_ids.sort()
+
+    if args.new_extraction_date is None:
+        if len(new_chunks_dates) > 1:
+            msg = ("Multiple dates extracted from new chunks filenames")
+            raise ValueError(msg)
+            del msg
+
+        new_date = new_chunks_dates.pop()
+        new_extraction_date = arrow.get(new_date, 'YYYYMMDD')
     else:
         new_extraction_date = arrow.get(args.new_extraction_date)
+
     assert (new_extraction_date > DATE_START and \
                 new_extraction_date < DATE_NOW)
 
-   
+    assert ( new_extraction_date > old_extraction_date )
+
+    selected_intervals = select_intervals(
+        (base_chunk.pageid_first, base_chunk.pageid_last),
+        new_chunks_ids
+        )
+
+    new_chunk_glob = (new_chunk_pattern
+                      .format(lang=base_chunk.lang,
+                               date=new_extraction_date.format('YYYYMMDD'),
+                               historyno='*',
+                               pageid_first='*',
+                               pageid_last='*',
+                               dumpext='*',
+                               ext='*')
+                      )
+
+    selected_chunks = set()
+    for cf in glob.glob(os.path.join(new_extractions_dir.as_posix(),
+                                     new_chunk_glob
+                                     )):
+        cf_basename = os.path.basename(cf)
+        new_match = re_chunk.match(cf_basename)
+        if new_match:
+            new_chunk = extract_name_elements(new_match)
+            if (new_chunk.pageid_first, new_chunk.pageid_last) in \
+                    selected_intervals:
+                selected_chunks.add(cf)
+
+    selected_chunks = sorted(selected_chunks, key=sort_chunks)
+    import ipdb; ipdb.set_trace()
+
     dump = csv.reader(dump)
     pages_generator = process_lines(
         dump,
         stats,
-        new_extractions_dir=new_extractions_dir,
+        selected_chunks=selected_chunks,
         ignore_newer=ignore_newer
         )
 
